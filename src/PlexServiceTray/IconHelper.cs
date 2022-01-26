616187fc7ce11c656d86c553402c0f0434204fb7
d8ahazard <d8ahazard@gmail.com>
2022-01-26 12:44:01 -0600

    Create Icon Helper class

---
+++ src/PlexServiceTray/IconHelper.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using System.Windows.Media.Imaging;
+
+namespace PlexServiceTray; 
+
+public static class IconHelper {
+	public static BitmapImage? GetIcon() {
+		var assembly = Assembly.GetExecutingAssembly();
+		var resource = assembly.GetManifestResourceStream("PlexServiceTray.PlexService.ico");
+		if (resource == null) {
+			return null;
+		}
+		var bitmapImage = new BitmapImage {
+			CreateOptions = BitmapCreateOptions.PreservePixelFormat
+		};
+		bitmapImage.BeginInit();
+		bitmapImage.StreamSource = resource;
+		bitmapImage.EndInit();
+		bitmapImage.Freeze();
+		resource.Close();
+		return bitmapImage;
+	}
+	
+	
+}
